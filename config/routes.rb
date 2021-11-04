d58d30056eb83db023dcd1a3ded10f590b41d79a
DungeonMaster666 <m.grishajevs@gmail.com>
2021-11-05 01:09:55 +0200

    v1.6 vrode rabotaet

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'home/index_2'
   get 'home/index'
   root 'home#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
