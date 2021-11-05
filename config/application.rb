c5b5abb96d32aa1e5e5a48553931c4f383a9396c
DungeonMaster666 <m.grishajevs@gmail.com>
2021-11-05 03:16:07 +0200

    v1.7 almost final, homepage is coming

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Project
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Riga"
+    config.time_zone = "Riga"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     #
   end
